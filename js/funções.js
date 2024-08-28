71466e6ef7f130cdaeee5f6bcc1bf244ac413802
Nathan099-dev <tonininathan8@gmail.com>
2024-08-28 10:33:58 -0300

    removendo validações do js

--- js/funções.js
+++ js/funções.js
@@ -16,7 +16,6 @@ let modal = {
 function cadastro(){
 	const nome_usuario = nome.value;
 	const email_usuario = email.value;
-	const telefone_usuario = telefone.value;
 	const senha_usuario = senha.value;
 	const confirmar_senha_usuario =  confirm.value
 
@@ -39,13 +38,10 @@ function cadastro(){
 		else if (email_usuario.indexOf('@').value == -1 || email_usuario.indexOf('.').value == -1) {
 		window.alert('email não reconhecido. Tente novamente')
 
+
 		}
 		
-		else if (telefone_usuario.value == null) {
-			window.alert('Campo inválido');
-			telefone_usuario.style.borderColor = '#ff0000';
-
-		}else if (senha_usuario == null) {
+		else if (senha_usuario == null) {
 			window.alert('Campo inválido. Crie uma senha')
 			senha_usuario.style.borderColor = '#ff0000';
 
@@ -55,12 +51,6 @@ function cadastro(){
 			window.alert('Sua senha precisa ter o mínimo de 8 caracteres')
 			senha_usuario.style.borderColor = '#ff0000';
 
-		}
-		
-		else if (confirmar_senha_usuario.value == null) {
-			window.alert('Campo inválido')
-			confirmar_senha_usuario.style.borderColor = '#ff0000';
-			
 		}
 		
 		else if (confirmar_senha_usuario != senha_usuario) {
