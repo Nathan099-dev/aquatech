7e34cd5491852fa852a8f47fdc4977f8c4650645
Nathan099-dev <tonininathan8@gmail.com>
2024-08-28 09:23:20 -0300

    deletando arquivo em js

--- js/login.js
+++
@@ -1,9 +0,0 @@
-function login(){
-    const email_usuario = email.value;
-    const senha_usuario = senha.value;
-
-    if (email_usuario.value == null) {
-        window.alert('Campo inválido');
-        email_usuario.style.borderColor = 'red'
-    }
-}
